9e72d6933a25920e010c61682a5fdbe1f7edcda2
Daniel <daniel.maers@gmail.com>
2021-07-06 19:32:15 -0300

    Revert "Update ProductCard.js"
    This reverts commit 0e1417377d7d7ac144f1e62cb546640e203d789c.

--- front/src/components/ProductCard/ProductCard.js
+++ front/src/components/ProductCard/ProductCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./ProductCard.css";
+import NavLink from "react-router-dom";
 
 export default function ProductCard(product) {
   return (
     <div className="card">
       <img src={product.img} alt="" className="productImg" />
-      <p>{product.name}</p>
+      <NavLink to="">{product.name}</NavLink>
       <p>{product.price}</p>
     </div>
   );
