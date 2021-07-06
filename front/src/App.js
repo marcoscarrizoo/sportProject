f74f1ec02c77eb8028c5ab1e6503f9b0ddc7de7b
Marcos Carrizo <marcoscarrizo7@gmail.com>
2021-07-06 16:44:01 -0300

    componente planes

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Plans from './components/Plans/Plans';
 
 function App() {
   return (
     <React.Fragment>
       <Route path='/' component={NavBar}/>
+      <Route path='/asociate' component={Plans}/>
     </React.Fragment>
   );
 }
