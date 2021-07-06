0e1417377d7d7ac144f1e62cb546640e203d789c
Daniel <daniel.maers@gmail.com>
2021-07-06 18:41:01 -0300

    Update ProductCard.js

--- front/src/components/ProductCard/ProductCard.js
+++ front/src/components/ProductCard/ProductCard.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./ProductCard.css";
-import NavLink from "react-router-dom";
 
 export default function ProductCard(product) {
   return (
     <div className="card">
       <img src={product.img} alt="" className="productImg" />
-      <NavLink to="">{product.name}</NavLink>
+      <p>{product.name}</p>
       <p>{product.price}</p>
     </div>
   );
