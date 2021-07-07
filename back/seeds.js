e2040adbb79fe3a1d5606e7429939c19d347aefc
Knut <kwaale@github.com>
2021-07-07 10:25:50 -0300

    Add products

--- back/seeds.js
+++ back/seeds.js
@@ -8,34 +8,34 @@ const products = [{
     price: 2250.00,
     stock: 3,
     categories: [{
-        name:"ropa",
+        name: "ropa",
         image: "https://http2.mlstatic.com/D_NQ_NP_756133-MLA41827275963_052020-O.webp"
 
     }]
-}, {
+},{
     name: "Ena Protein Bar Caja X 16u 46g - Barras De Proteina Sabores ",
     description: "ENA PROTEIN BAR",
-    image: [
+    images: [
         "https://http2.mlstatic.com/D_NQ_NP_707077-MLA40743470620_022020-O.webp",
         "https://http2.mlstatic.com/D_NQ_NP_779555-MLA40743624545_022020-O.webp",
     ],
     price: 1315.00,
     stock: 4,
     categories: [{
-        name:"zapatillas",
+        name: "zapatillas",
         image: "https://blog.runnics.com/wp-content/uploads/2019/10/zapatillas-correr-tipos-1024x529-1.jpg"
     }]
-}, {
+},{
     name: "Fish Oil 1200mg Nature's Bounty Aceite De Pescado Omega 3",
     description: "Un suplemento dietario que contiene ácidos grasos omega-3, incluidos EPA y DHA.",
-    image: [
+    images: [
         "https://http2.mlstatic.com/D_NQ_NP_904826-MLA44008957239_112020-O.webp",
         "https://http2.mlstatic.com/D_NQ_NP_779555-MLA40743624545_022020-O.https://http2.mlstatic.com/D_NQ_NP_687475-MLA41534188094_042020-O.webp",
     ],
-    price: 1011.00,
+    price: 1011.10,
     stock: 5,
     categories: [{
-        name:"zapatillas",
+        name: "zapatillas",
         image: "https://blog.runnics.com/wp-content/uploads/2019/10/zapatillas-correr-tipos-1024x529-1.jpg"
     }]
 }
