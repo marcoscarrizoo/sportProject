6b8fd7592878f3b755f5ec9b0c2097955a236850
briancalamar <briansantaran@gmail.com>
2021-07-07 13:37:10 -0300

    created catalogo

--- front/src/App.js
+++ front/src/App.js
@@ -3,12 +3,15 @@ import {Route} from 'react-router-dom'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Plans from './components/Plans/Plans';
+import Catalogo from './components/Catalogo/Catalogo';
+import { products } from './seeds';
 
 function App() {
   return (
     <React.Fragment>
       <Route path='/' component={NavBar}/>
       <Route path='/asociate' component={Plans}/>
+      <Route path='/productos' render={() => <Catalogo products={products}/>}/>
     </React.Fragment>
   );
 }
