96dcad60a452ce53f3f09e6ddc177c5f05449e86
Marcos Carrizo <marcoscarrizo7@gmail.com>
2021-07-07 14:11:13 -0300

    productDetail solo estetico - marcos

--- front/src/App.js
+++ front/src/App.js
@@ -3,12 +3,14 @@ import {Route} from 'react-router-dom'
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Plans from './components/Plans/Plans';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
   return (
     <React.Fragment>
       <Route path='/' component={NavBar}/>
       <Route path='/asociate' component={Plans}/>
+      <Route path='/detalle' component={ProductDetail}/>
     </React.Fragment>
   );
 }
