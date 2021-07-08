7d9e83b55ec1e8c2e1da6931338edb2a08a866a6
Marcos Carrizo <marcoscarrizo7@gmail.com>
2021-07-07 22:07:59 -0300

    productDetailById

--- back/src/routes/productRoutes.js
+++ back/src/routes/productRoutes.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const { getProducts } = require("../controllers/productController");
 const { createReview } = require("../controllers/reviewController");
 const { createProducts } = require("../controllers/productController");
+const {getProductById} = require('../controllers/productController')
 
 const router = Router();
 
 router.get("/", getProducts);
 router.post("/addReview", createReview);
 router.post("/addProducts", createProducts);
+router.get("/:id" , getProductById)
 
 module.exports = router;
--- front/src/redux/reducers/productReducer.js
+++ front/src/redux/reducers/productReducer.js
@@ -1,14 +1,19 @@
 //actions
-
+import { GET_PRODUCT_DETAIL } from "../actions/productsActions"
 
 //states 
 const initialState = {
-
+productDetail: []
 }
 
 
 export default function reducer(state = initialState, action) {
     switch(action.type) {
+        case GET_PRODUCT_DETAIL:
+            return {
+                productDetail : action.payload
+            }
+        
         default:
             return state 
     }
