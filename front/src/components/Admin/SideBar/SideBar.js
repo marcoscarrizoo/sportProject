643ea50d6393457ae740dce13db498f57f1d2fed
briancalamar <briansantaran@gmail.com>
2021-07-13 21:20:31 -0300

    admin panel in progress

---
+++ front/src/components/Admin/SideBar/SideBar.js
@@ -0,0 +1,13 @@
+import ControlledAccordions from "./Accordions";
+import './SideBar.css'
+
+
+export default function SideBar(){
+
+    return (
+        <div className="sidebar">
+            <h2> Sport GYM</h2>
+            <ControlledAccordions/>
+        </div>
+    )
+}
