6c3c45adb38c5ae76ce985a5a4779e62f5b940c4
Daniel <daniel.maers@gmail.com>
2021-07-15 17:30:32 -0300

    cambiado model usuario

--- back/src/models/User.js
+++ back/src/models/User.js
@@ -4,9 +4,9 @@ module.exports = (sequelize) => {
   sequelize.define(
     "user",
     {
-      uuid: {
+      id: {
         type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
         primaryKey: true,
       },
       email: {
@@ -18,35 +18,15 @@ module.exports = (sequelize) => {
       password: {
         type: DataTypes.STRING,
       },
-      isAdmin: {
-        type: DataTypes.BOOLEAN,
-        default: false,
-      },
-      shippingCost: {
-        type: DataTypes.FLOAT,
-      },
-      shippingAddress: {
-        type: DataTypes.STRING,
-      },
-      shippingZip: {
-        type: DataTypes.STRING,
-      },
-      shippingCity: {
-        type: DataTypes.STRING,
-      },
-      shippingState: {
-        type: DataTypes.STRING,
+      userType: {
+        type: DataTypes.ENUM("S", "A", "B", "C"), // usuario S superusuario, A admin, B usuario normal. C invitado / usuario no registrado
+        default: "B", // por defecto se crean usuarios normales
+        allowNull: true,
       },
       firstName: {
         type: DataTypes.STRING,
       },
       lastName: {
-        type: DataTypes.STRING,
-      },
-      comments: {
-        type: DataTypes.STRING,
-      },
-      paymentDetails: {
         type: DataTypes.STRING,
       },
     },
---
+++ front/src/components/Cart/Cart.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+// const cart=[
+//     {
+//         image: "image1",
+//         name: "mancuerna",
+//         precio: 300,
+//         cantidad: 1,
+//         descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in laoreet felis. In fermentum massa pharetra egestas rutrum. Sed varius nibh vitae libero bibendum, a imperdiet diam mattis. Duis volutpat libero non odio viverra, id semper elit convallis. Sed molestie nibh nec nulla elementum sodales. Phasellus maximus sapien a tincidunt elementum. Proin vitae mauris non quam sollicitudin semper sit amet quis mauris. "
+//     },
+//     {
+//         image:,
+//         name:,
+//         precio:,
+//         cantidad:,
+//         descripcion:
+//     },
+//     {
+//         image:,
+//         name:,
+//         precio:,
+//         cantidad:,
+//         descripcion:
+//     }
+// ]
+
+export default function Cart() {
+  //const cart = useSelector((store) => store.user.cart);
+  return <div>{/* {cart?} */}</div>;
+}
