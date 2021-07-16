2f61b2c4cf5db25e1f93d80c305ffa766fa69d14
briancalamar <briansantaran@gmail.com>
2021-07-15 22:42:04 -0300

    alert after delete product created

--- back/index.js
+++ back/index.js
@@ -32,7 +32,7 @@ conn.sync({ force: true }).then(() => {
     console.log(`listening at PORT ${process.env.PORT}`);
     //Esta funcion carga productos del archivo seeds en la DB
 
-    //await createProductsSeeds();
+    await createProductsSeeds();
 
   });
 });
