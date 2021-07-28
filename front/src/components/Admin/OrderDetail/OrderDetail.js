75c895d78245ba8b2ff972f5191d8703964a9005
briancalamar <briansantaran@gmail.com>
2021-07-28 19:18:09 -0300

    orderDetail

--- front/src/components/Admin/OrderDetail/OrderDetail.js
+++ front/src/components/Admin/OrderDetail/OrderDetail.js
@@ -7,7 +7,7 @@ import './OrderDetail.css'
 
 export default function OrderDetail() {
     console.log("entro al componenteeee")
-    const {orderid} = useParams()
+    const { orderid } = useParams()
     const dispatch = useDispatch()
     const order = useSelector(store => store.adm.orderDetail)
 
@@ -17,23 +17,30 @@ export default function OrderDetail() {
     }, [dispatch])
     return (
         <div className="OrderDetail">
-            <div className="">
-                <div>
+            <div className="od-info">
+                <div className="od-info-c">
+                    <h4>Datos de orden</h4>
                     <p>{order?.id}</p>
                     <p>{`Estado de orden: ${order?.orderState}`}</p>
-                    <p>{`Estado de orden: ${order?.paymentState}`}</p>
-                    <div>
-                    <p>{`Estado de orden: ${order?.shippingState}`}</p>
-                    <p>{`Estado de orden: ${order?.shippingAddres}`}</p>
-                    <p>{`Estado de orden: ${order?.shippingZip}`}</p>
-                    <p>{`Estado de orden: ${order?.shippingCity}`}</p>
-
-                    </div>
-
+                    <p>{`Estado de pago: ${order?.paymentState}`}</p>
                 </div>
-                <div>
-
+                <div className="od-info-c">
+                    <h4>Datos de envio</h4>
+                    <p>{`Estado de envio: ${order?.shippingState}`}</p>
+                    <p>{`Direccion de envio: ${order?.shippingAddres}`}</p>
+                    <p>{`Codigo postal: ${order?.shippingZip}`}</p>
+                    <p>{`Ciudad: ${order?.shippingCity}`}</p>
                 </div>
+                <div className="od-info-c">
+                    <h4>Datos del comprador</h4>
+                    <p>{`ID: ${order?.user.id}`}</p>
+                    <p>{`Ciudad: ${order?.user.firstName}`}</p>
+                    <p>{`Ciudad: ${order?.user.lastName}`}</p>
+                    <p>{`Ciudad: ${order?.user.email}`}</p>
+                </div>
+            </div>
+            <div className="od-products">
+
             </div>
         </div>
     )
